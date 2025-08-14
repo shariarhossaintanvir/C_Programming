e94c6d1c06cad11a79495702684d46436c616218
Shariar Hossain Tanvir <bangboombro629@gmail.com>
2025-08-15 00:46:08 +0600

    Add files via upload

---
+++ Add integer.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a ,b ;
+    printf("First Number\n");
+    scanf("%d", &a);
+    printf("Second Number\n");
+    scanf("%d", &b);
+    printf("Sum is %d\n", a + b);
+    return 0;
+}
